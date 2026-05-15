67a034b30835e70e3ff65defdccd33308c8a8db1
kimminjae <kimminjae0528@gmail.com>
2026-05-15 16:57:17 +0900

    object, map, block class 제작

--- Object.h
+++ Object.h
@@ -1,10 +1,39 @@
 #pragma once
-class Object
-{
-	int abx;
-	int aby;
+#include <Windows.h>
+
+class Object {
+private:
+    // 원본 init()에서 사용한 화면 기준점
+    // ab_x = 5, ab_y = 1;
+    int abx;
+    int aby;
+
 public:
-	int moveCursor(int x, int y);
-	int changeColor(int color);
+    Object();
+
+    // 원본 gotoxy(int x, int y)
+    void moveCursor(int x, int y);
+
+    // 원본 SetColor(int color)
+    void changeColor(int color);
 };
 
+Object::Object() {
+    abx = 5;
+    aby = 1;
+}
+
+void Object::moveCursor(int x, int y) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    COORD pos;
+    pos.X = x;
+    pos.Y = y;
+
+    SetConsoleCursorPosition(hConsole, pos);
+}
+
+void Object::changeColor(int color) {
+    static HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, color);
+}
